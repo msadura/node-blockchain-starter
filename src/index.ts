8d95842a9e79adf4666c1ddd91d81b834cfb92bf
Marek <m.sadura@outlook.com>
2022-02-11 16:06:54 +0100

    Update deps

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,20 @@ dotenv.config();
 
 import { connectProvider } from '@app/blockchain/provider';
 import { connectWallet } from '@app/blockchain/wallet';
+import { ethers } from 'ethers';
 
 const PORT = process.env.PORT || 3002;
 
 async function main() {
-  await connectProvider();
-  await connectWallet();
+  // await connectProvider();
+  // await connectWallet();
   // const app = express();
   // app.listen(PORT, () => {
   //   console.log(`[ server ] ready on port ${PORT}`);
   // });
+
+  const wallets = new Array(100).fill(true).map(() => ethers.Wallet.createRandom().address);
+  console.log('🔥', wallets);
 }
 
 main();
